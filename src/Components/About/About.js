dc3f4fbd8bd7972dbd12d2564f974af0ec1d435f
Hem Raj Rana <ryuu817@gmail.com>
2021-01-24 18:15:09 +0530

    edited about
    added few more lines; added email link, added resume download link

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import resume from '../../assets/document/cv-itshrr17.pdf'
 
 import {ReactComponent as GithubIcon} from '../../assets/icons/github.svg'
 
@@ -7,36 +8,45 @@ const About = ({itemIndex}) => {
 
      return (
           <div className="flex column">
-               <div className="flex tagline align-center">
+               <strong className="flex tagline align-center">
                     I am a self-taught frontend web developer. <br/>I love building user interfaces.
-               </div>
-               <div className="paragraph fadeIn">
-                    <h2> Where it all begins </h2>
+               </strong>
+               
+               <article className="paragraph fadeIn">
+                    <h1> Where it all begins </h1>
                     <p> 
-                         <i>
                          When I was a kid, I got fascinated by websites.<br/>
                          I developed a strong feeling to make these things by myself.<br/>
                          But I did not know how to build websites at that time.<br/>
                          I started with "mobile website builders" websites.<br/>
                          I had no idea where to get started. So, I found myself fiddling with Html, CSS, and a little Javascript.
-                         </i>
                     </p>
 
                     <br/>
 
-                    <h2> Fast forward to College </h2>
+                    <h1> Fast forward to College </h1>
                     <p> 
-                         <i>
                          I started learning web development (front-end) through online resources. <br/>
                          Though I wasn't that serious, it was no more than a hobby. <br/>
                          And in the last year of my college, I found myself spending more time fiddling with web technologies.<br/>
                          That was when I realized the hobby I started when I was a kid; has become my passion.<br/>
-                         </i>
                     </p>
-               </div>
-               <br/>
-               <div className="flex justify-center align-center">
-                   <a href="https://github.com/hrr817"><span className="icon fadeIn"><GithubIcon/></span></a>
+
+                    <br/>
+
+                    <h1> Present </h1>
+                    <p> 
+                         I am learning web technologies and improving my current skills. <br/>
+                         Also, looking for intership/jobs as a front-end developer. 
+                         <br/> <br/>
+                         I love playing chess, reading comics, novels and books, learning new skills, listening to jpop and watching anime.
+                         <br/><br/>
+                         Have something in mind, make use of words, write me at <a href="mailto:itshrr17@gmail.com" style={{color: 'inherit', transition: 'color 0.2s linear'}}>itshrr17@gmail.com</a> 
+                    </p>
+               </article>
+               <div className="flex column justify-center align-center">
+                    <a style={{padding: '1em', color: 'inherit', transition: 'color 0.2s linear'}} href={resume} download>Click here to download my resume</a>
+                    <a href="https://github.com/hrr817"><span className="icon fadeIn"><GithubIcon/></span></a>
                </div>
           </div>
      )

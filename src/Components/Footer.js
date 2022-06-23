582d65cb95fed395ad370fc428a0a50137c289ee
Hem Raj Rana <hemraj.rana@hoolime.co>
2022-06-23 11:30:32 +0530

    chanegs

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
 const Footer = () => {
+     const year = new Date().getFullYear()
+
      return (
-          <div className="footer"> itshrr17 © 2021. All Rights Reserved. </div>
+          <div className="footer"> itshrr17 © {year} </div>
      )
 }
 
